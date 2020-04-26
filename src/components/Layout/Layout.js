c938e509884ab71790d7a7110316a489ee11ef47
Andrew N.Romanov <andrew.n.romanov@gmail.com>
2020-04-26 18:53:24 +0300

    Mobile menu.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import styles from './Layout.module.css';
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <main className={styles.Content}>
-      {props.children}
-    </main>
-  </Aux>
-);
+class Layout extends Component  {
+  state = {
+    showSideDrawer: false
+  }
+
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return {showSideDrawer: !prevState.showSideDrawer}
+    });
+  }
+
+  render () {
+    return (
+      <Aux>
+        <Toolbar 
+          drawerToggleClicked={this.sideDrawerToggleHandler}/>
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerToggleHandler}/>
+        <main className={styles.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    )
+  }
+}
 
 export default Layout;
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import styles from './SideDrawer.module.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
+
+const SideDrawer = (props) => {
+  let additionalClass = props.open ? styles.Open : styles.Close;
+  const attachedClasses = [styles.SideDrawer, additionalClass];
+  
+  return (
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(' ')}>
+        <div className={styles.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Aux>
+  );
+};
+
+export default SideDrawer;
